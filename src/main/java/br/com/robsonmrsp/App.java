32d36f0268e9b920baa374a7d24b4074a63558e7
Marcio Robson <robsonmrsp@gmail.com>
2021-02-22 22:54:41 -0300

    Finalize the implementation of HelloWorldServiceImp and add it to server

--- src/main/java/br/com/robsonmrsp/App.java
+++ src/main/java/br/com/robsonmrsp/App.java
@@ -1,11 +1,26 @@
 package br.com.robsonmrsp;
 
+import java.io.IOException;
+
+import br.com.robsonmrsp.server.HelloWorldServiceImp;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 /**
  * Hello world!
- *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println("Hello World!");
+		Server server = ServerBuilder.forPort(8083).addService(new HelloWorldServiceImp()).build();
+
+		server.start();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+			server.shutdown();
+		}));
+
+		server.awaitTermination();
 	}
 }
--- src/main/java/br/com/robsonmrsp/server/HelloWorldServiceImp.java
+++ src/main/java/br/com/robsonmrsp/server/HelloWorldServiceImp.java
@@ -5,18 +5,21 @@ import br.com.robsonmrsp.proto.HelloWorldMessageResponse;
 import br.com.robsonmrsp.proto.HelloWorldServiceGrpc.HelloWorldServiceImplBase;
 import io.grpc.stub.StreamObserver;
 
-public class GreetingServer extends HelloWorldServiceImplBase {
+public class HelloWorldServiceImp extends HelloWorldServiceImplBase {
 
 	@Override
 	public void sayHello(HelloWorldMessageRequest request, StreamObserver<HelloWorldMessageResponse> responseObserver) {
-		super.sayHello(request, responseObserver);
+//		super.sayHello(request, responseObserver);
 		String myName = request.getMyName();
 
-		HelloWorldMessageResponse build = HelloWorldMessageResponse.newBuilder().setGreeting("Hello, " + myName).build();
+		HelloWorldMessageResponse build = HelloWorldMessageResponse
+				.newBuilder()
+				
+				.setGreeting("Hello, " + myName)
+				.build();
 
 		responseObserver.onNext(build);
-		responseObserver.onCompleted();
-		;
 
+		responseObserver.onCompleted();
 	}
 }
